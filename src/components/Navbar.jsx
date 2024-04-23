fd17658157be7980a52a6038609e41130ef4f731
justin etienne <juetienne7@gmail.com>
2024-04-23 11:33:18 +0900

    navbar started

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,46 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <div  className="flex gap-5 my-3">
-        <div className="text-xl">Navbar</div>
-        <ul className="flex gap-3 text-sky-500 items-center">
+      <div className="bg-pink-200">
+        <div className="flex gap-5 h-20 mx-9 justify-between ">
+          <div className="flex items-center gap-1">
+            <img className="w-16" src="./public/images/logo.png" alt="" />
+            <h1>tokuyukikaku</h1>
+          </div>
+          <div className="flex flex-col items-end">
+            <div>
+              <div className="flex">
+                <img className="w-16" src="./public/images/logo.png" alt="" />
+                <p>number</p>
+              </div>
+              <div className="flex">
+                <img className="w-16" src="./public/images/logo.png" alt="" />
+                <p>Heures</p>
+              </div>
+            </div>
+            <div>
+              <ul className="flex gap-6">
+                <li>私たちについて</li>
+                <li className="group hover:text-sky-800 ">サービス
+                  <ul className="hidden group-hover:block">
+                    <li>訪問介護</li>
+                    <li>介護タクシー</li>
+                    <li>デイサービス</li>
+                    <li>居宅介護支援</li>
+                  </ul>
+                </li>
+                <li>お問い合わせ</li>
+                <li>採用情報</li>
+              </ul>
+            </div>
+          </div>
+          {/* <ul className="flex gap-3 text-sky-500 items-center">
           <li className="hover:text-yellow-400 transition-colors delay-75"><Link to="/">Home</Link></li>
           <li className="hover:text-yellow-400 transition-colors delay-75"><Link to="/about">About</Link></li>
-        </ul>
+        </ul> */}
+        </div>
       </div>
+
     </>
   );
 };
