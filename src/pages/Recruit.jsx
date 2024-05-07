1ae5ae276846b2b7954cbd79509950dd00840296
justin etienne <juetienne7@gmail.com>
2024-05-07 13:48:15 +0900

    recruit responsive started

--- src/pages/Recruit.jsx
+++ src/pages/Recruit.jsx
@@ -3,13 +3,18 @@ const Recruit = () => {
     <>
       <div className="px-6 2md:px-12 lg:px-24 xl:px-28 py-6 lg:py-12">
         <div className="flex gap-10">
-          <div className="w-3/5">
+          <div className="sm:w-4/5 2md:w-3/5">
             <h1 className="text-2xl text-darkpink mb-2">採用情報</h1>
             <div>
               <h1 className="text-xl mb-2">お仕事内容</h1>
               <p className="mb-2">介護職員（常勤・非常勤ヘルパー）及び介護タクシードライバーを募集中</p>
               <p className="mb-2">朝食･昼食･夕食作り､各所掃除･洗濯･おむつ交換､お買物､入浴介助などの介護業務を､シフト制で分担して対応していきます｡</p>
               <p className="text-[px]">※直行直帰OK！</p>
+            </div>
+            <div className="flex w-full h-32 mt-6 sm:hidden">
+              <div className="bg-[url('./illustrations/helper2.jpg')] bg-cover bg-top w-1/3"></div>
+              <div className="bg-[url('./illustrations/helper1.jpg')] bg-cover bg-top w-1/3"></div>
+              <div className="bg-[url('./illustrations/helper3.jpg')] bg-cover bg-top w-1/3"></div>
             </div>
             <div className="mt-6">
               <h1 className="text-xl mb-2">募集要項</h1>
@@ -31,28 +36,39 @@ const Recruit = () => {
                       <ul>
                         <li>非常勤ヘルパー 8：00～18：30の間でシフト制 ＜週1日～･1日1h～勤務OK＞</li>
                         <li className="mb-1">常勤ヘルパー 8：00～17：00又は9：00～18：00（週休二日制）</li>
-                        <li>★家庭環境等､考慮致します★直行直帰可</li>
+                        <li>★家庭環境等､考慮致します</li>
+                        <li>★直行直帰可</li>
                       </ul>
                     </td>
                   </tr>
                   <tr>
                     <th className="bg-lightpink border border-darkstone p-2 text-start" scope="row">対象となる方</th>
-                    <td className="bg-white p-2 border border-darkstone" >･要普通自動車免許(AT限定可)･ヘルパー2級以上必須（経験は問いません､資格取得見込み者もご相談ください）･学歴不問</td>
-                                    </tr>
+                    <td className="bg-white p-2 border border-darkstone" >
+                      <ul>
+                        <li>･要普通自動車免許(AT限定可)</li>
+                        <li>･ヘルパー2級以上必須（経験は問いません､資格取得見込み者もご相談ください）</li>
+                        <li>･学歴不問</li>
+                      </ul>
+                      </td>
+                  </tr>
                   <tr>
                     <th className="bg-lightpink border border-darkstone p-2 text-start" scope="row">特別手当</th>
-                    <td className="bg-white p-2 border border-darkstone">弊社は東京都の介護職員処遇改善加算Ⅰ・介護職員等特定処遇改善加算Ⅱ及び介護職員ベースアップ等加算を取得しております。
-                      処遇改善(特別手当)手当として、常勤ヘルパーは毎月66,000円～150,000円(経験及び勤務年数による)支給。
-                      非常勤ヘルパーは訪問1件に付き300円支給。（自費利用の場合は、特別手当がつきません。）</td>
+                    <td className="bg-white p-2 border border-darkstone">
+                      <ul>
+                        <li>弊社は東京都の介護職員処遇改善加算Ⅰ・介護職員等特定処遇改善加算Ⅱ及び介護職員ベースアップ等加算を取得しております。</li>
+                        <li>処遇改善(特別手当)手当として、常勤ヘルパーは毎月66,000円～150,000円(経験及び勤務年数による)支給。
+                      非常勤ヘルパーは訪問1件に付き300円支給。（自費利用の場合は、特別手当がつきません。）</li>
+                      </ul>
+                    </td>
                   </tr>
                 </tbody>
               </table>
             </div>
           </div>
-          <div className="w-2/5">
-            <div className="bg-[url('./illustrations/helper2.jpg')] bg-cover bg-center h-1/3"></div>
-            <div className="bg-[url('./illustrations/helper1.jpg')] bg-cover bg-center h-1/3"></div>
-            <div className="bg-[url('./illustrations/helper3.jpg')] bg-cover bg-center h-1/3"></div>
+          <div className="hidden sm:block w-1/5 2md:w-2/5">
+            <div className="bg-[url('./illustrations/helper2.jpg')] bg-cover bg-top h-1/3"></div>
+            <div className="bg-[url('./illustrations/helper1.jpg')] bg-cover bg-top h-1/3"></div>
+            <div className="bg-[url('./illustrations/helper3.jpg')] bg-cover bg-top h-1/3"></div>
           </div>
         </div>
       </div>
