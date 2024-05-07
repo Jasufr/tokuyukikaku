9688b232b6cc7c300f4f2075a905ad02171876d7
justin etienne <juetienne7@gmail.com>
2024-05-07 09:19:40 +0900

    about page responsive

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,22 +5,22 @@ const About = () => {
     <>
       <div className="about">
         <div className="about-us px-6 2md:px-12 lg:px-24 xl:px-28 py-6 lg:py-12 flex gap-10 items-center">
-          <div className="w-3/5">
+          <div className="md:w-3/4 2md:w-3/5">
             <h1 className="text-2xl mb-2 text-darkpink">私たちについて</h1>
             <p>当社は、高齢者や障がいを持つ方々、そしてそのご家族の方々に寄り添い、心身の健康と幸福をサポートすることを使命としています。私たちは、利用者様、ご家族の皆様、地域のコミュニティ、そして当社のスタッフ全員の幸福と福祉を真摯に考えています。
               介護に取り組む私たちは、慎重な考慮と行動に基づいて、個々のニーズを理解し、満たすことを最優先にしています。一人ひとりの利用者様に最適なケアプランを提供し、彼らの生活を豊かで充実したものにすることを誓います。
               また、私たちは真の人間関係を築き、お互いに学び合い、支え合う環境を大切にしています。地域のコミュニティとの協力を通じて、お互いに成長し、より良い未来を築いていくことを目指しています。</p>
           </div>
-          <div className="bg-[url('./images/dayservice2.jpg')] bg-center bg-cover rounded-md shadow-md w-2/5 min-h-80"></div>
+          <div className="bg-[url('./images/dayservice2.jpg')] bg-center bg-cover rounded-md shadow-md hidden md:block w-1/4 2md:w-2/5 min-h-80"></div>
         </div>
         <div className="annai py-6 lg:py-12 bg-lightgray bg-opacity-30">
           <div className="px-6 2md:px-12 lg:px-24 xl:px-28">
             <h1 className="text-2xl mb-2 text-darkpink">会社案内</h1>
-            <div className="flex gap-10">
-              <table className="w-3/4">
+            <div className="2md:flex gap-10">
+              <table className="mb-4 2md:mb-0 w-full 2md:w-3/4">
                 <tbody>
                   <tr>
-                    <th className="bg-lightpink p-2 text-start border border-darkstone" scope="row">社名（事業所番号）</th>
+                    <th className="bg-lightpink p-2 text-start border border-darkstone min-w-24" scope="row">社名（事業所番号）</th>
                     <td className="bg-white p-2 border border-darkstone">合同会社徳有企画 （139540041）</td>
                   </tr>
                   <tr>
@@ -41,7 +41,7 @@ const About = () => {
                   </tr>
                 </tbody>
               </table>
-              <div className="w-1/4 min-h-72">
+              <div className="2md:w-1/4 min-w-64 h-64 xs:h-80 2md:min-h-72">
                 <iframe className="rounded-md shadow-md" style={{ border: '0', height: '100%', width: '100%' }} loading="lazy" allowFullScreen src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJRbVMFADlGGART5nk37jmu4c&key=${mapKey}`}></iframe>
 
               </div>
