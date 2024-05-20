37cb1e01cc467aec53090baeafddcb8a887a862e
justin etienne <juetienne7@gmail.com>
2024-05-20 14:52:34 +0900

    ippan taxi content added

--- src/pages/ServiceTaxi.jsx
+++ src/pages/ServiceTaxi.jsx
@@ -90,26 +90,18 @@ const ServiceTaxi = () => {
           </Tabs>
           {/* 一般タクシー */}
           <div className="flex items-center gap-5 2md:gap-10 mb-3 xs:mb-4">
-            <h1 className="text-2xl text-darkpink text-nowrap">訪問介護</h1>
+            <h1 className="text-2xl text-darkpink text-nowrap">一般介護タクシー</h1>
             <div className="bg-[url('./images/kuruma2.jpg')] bg-center bg-cover w-full h-14 sm:h-20 rounded-md shadow-md"></div>
           </div>
           <Tabs id="2" visibleTab={visibleTab2} setVisibleTab={setVisibleTab2}>
             <div className={`shoukai ${visibleTab2 === "tab1" ? "block" : "hidden"} mt-4`}>
-              <h1 className="mb-3 text-xl">訪問介護とは</h1>
-              <p className="mb-1">訪問介護員（ホームヘルパー）などがご自宅に訪問し、介護や日常生活上のお世話を行うサービスです。</p>
-              <p>ホームヘルパーなどのスタッフが、ご自宅を訪問して必要なサービスを行います。食事や排せつなどの介助を行う「身体介護」、調理や掃除などを行う「生活援助」をはじめ、ご自宅での生活に関するサービスや日常生活でのアドバイスをいたします。 </p>
-              <div className="xs:flex gap-5 2md:gap-10 mt-4">
-                <div className="xs:w-1/2 mb-4 xs:mb-0">
-                  <h1 className="mb-3 text-xl">身体介護</h1>
-                  <p className="mb-2">日常的な介護を必要とする方に、身体機能向上のための適切なサービスをご提供いたします。</p>
-                  <p>食事、洗面、入浴、部分浴（洗髪、陰部・足部などのみの洗浄）、清拭（せいしき：身体を拭いて清潔にすること）、洗髪、排泄、衣類の着脱、床ずれの予防、体位変換・姿勢交換、ベッドメイキング、歩行、車いす等にかかわる介助。 </p>
-                </div>
-                <div className="xs:w-1/2">
-                  <h1 className="mb-3 text-xl">生活援助</h1>
-                  <p className="mb-2">ご利用者様が単身、ご家族がご病気などの場合に自立支援やご家族の負担軽減のために適切なサービスをご提供いたします。</p>
-                  <p className="mb-2"> 買物、調理、配膳、洗濯、掃除、衣類の整理、薬の受け取り等にかかわる介助。</p>
-                  <p>相談・助言・情報提供など。 </p>
-                </div>
+              <h1 className="mb-3 text-xl">一般介護タクシーとは</h1>
+              <p className="">西東京市を拠点に東京都全域介護保険外（自費利用）でも安心安全安い介護タクシーをご利用いただけます。</p>
+              <div className="mt-4">
+                <h1 className="mb-3 text-xl">ご利用対象</h1>
+                <p className="mb-2">（1）介護保険法第19条に規定する要介護認定又は要支援認定を受けている方。
+                  （2）身体障害者福祉法第4条に規定する身体障害者手帳の交付を受けている方。
+                  （3）上記（1）,（2）に該当する方のほか、肢体不自由、内部障害、知的障害及び精神障害その他の障害を有する等により単独での移動が困難な方であって、単独でタクシーその他の公共交通機関を利用することが困難な方。 </p>
               </div>
             </div>
 
