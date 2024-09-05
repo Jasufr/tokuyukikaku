4756e3fd6b1207ef18f01ab2cac946e395de78c8
justin etienne <juetienne7@gmail.com>
2024-09-05 12:51:43 +0900

    font options added, tabs title changed

--- src/components/TaxiPopUp.jsx
+++ src/components/TaxiPopUp.jsx
@@ -5,7 +5,7 @@ const TaxiPopUp = props => {
 
   return (
     <>
-      <div className="taxiPopUpBox fixed z-50 right-0 bottom-0">
+      <div className="taxiPopUpBox fixed z-40 right-0 bottom-0">
         <div className="taxiPopUp bg-darkpink bg-opacity-80 rounded-ss-2xl px-10 py-5 flex flex-col justify-center items-center relative">
           <h3 className="text-lg mb-2 text-lightpink [text-shadow:_0px_1px_1px_rgb(0_0_0_/_100%)]"><span className="text-[#ffe082]">介護タクシー</span>の情報はこちらです！</h3>
           <Link className="" to="/taxi"><p className="bg-lightgray px-10 py-2 rounded-md shadow-inner text-green font-bold hover:bg-darkstone hover:bg-opacity-70 hover:text-lightpink transition-colors duration-200">詳細</p></Link>
--- src/layout/Tabs.jsx
+++ src/layout/Tabs.jsx
@@ -12,7 +12,7 @@ const Tabs = ({ children, setVisibleTab, id }) => {
               <input type="radio" id={`radio-1-${id}`} name={`tabs-${id}`} className="hidden peer/radio-1" defaultChecked onClick={() => handleTabChange("tab1")} />
               <label className="tab h-full flex items-center justify-center peer-checked/radio-1:text-darkpink z-10 w-1/2 transition-all duration-200 sm:text-xl font-bold" htmlFor={`radio-1-${id}`}>サービス紹介</label>
               <input type="radio" id={`radio-2-${id}`} name={`tabs-${id}`} className="hidden peer/radio-2" onClick={() => handleTabChange("tab2")} />
-              <label className="tab h-full flex items-center justify-center peer-checked/radio-2:text-darkpink z-10 w-1/2 transition-all duration-200 sm:text-xl font-bold" htmlFor={`radio-2-${id}`}>利用方法</label>
+              <label className="tab h-full flex items-center justify-center peer-checked/radio-2:text-darkpink z-10 w-1/2 transition-all duration-200 sm:text-xl font-bold" htmlFor={`radio-2-${id}`}>利用方法・料金</label>
               {/* <input type="radio" id="radio-3" name="tabs" className="hidden peer/radio-3" onClick={() => handleTabChange("tab3")} />
               <label className="tab h-full flex items-center justify-center peer-checked/radio-3:text-darkpink z-10 w-1/3 transition-all duration-200 text-sm xs:text-xl font-bold" htmlFor="radio-3">料金</label> */}
               <span className="glider absolute flex justify-center items-center left-0 bg-darkpink bg-opacity-20 transition-all duration-500 ease-in-out h-full w-1/2 peer-checked/radio-1:translate-x-[0%] peer-checked/radio-2:translate-x-[100%] peer-checked/radio-3:translate-x-[200%] rounded-lg"></span>
